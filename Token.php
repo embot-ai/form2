a11d9a1b42a75db2c6d0d2d3201f2d24c80c1ba3
Serge Cornelissen <serge@embot.ai>
2019-05-05 15:48:05 +0200

    initial commit

--- Token.php
+++ Token.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2019 Serge Cornelissen
+ * Copyright 2019 Serge Cornelissen, Jelmer Idzenga
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
